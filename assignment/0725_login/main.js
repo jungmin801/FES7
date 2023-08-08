60a7b8749ec08cf36ae87211ab9458c079e1f6fd
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-08 17:03:34 +0900

    202030808 update markup & fix error

--- assignment/0725_login/main.js
+++ assignment/0725_login/main.js
@@ -5,26 +5,25 @@ const loginId = document.querySelector('.login-id');
 const password = document.querySelector('.password');
 const btnLogin = document.querySelector('.btn-login');
 
-let valueID = loginId.value;
-let valuePW = password.value;
-
-
 // 에러 메세지 띄우기
 function displayError(box, errorMessage, button){
-    const errorElement = document.createElement('p');
-    box.appendChild(errorElement);
-    button.classList.add('error-border');
+    let errorElement = box.querySelector('.error-input')
+    if(!errorElement){
+        errorElement = document.createElement('strong');
+        box.appendChild(errorElement);
 
-    errorElement.className = 'error-input'
-    errorElement.innerText = errorMessage;
-    errorElement.style.color = '#F4492E';
-    errorElement.style.marginTop = '6px';
+        errorElement.className = 'error-input'
+        errorElement.innerText = errorMessage;
+        errorElement.style.color = '#F4492E';
+        errorElement.style.marginTop = '6px';
 
+        button.classList.add('error-border');
+    }
 }
 
 // 에러 메세지 지우기(원래상태로 복구)
-function resetError(button){
-    let errorInput = document.querySelector('.error-input');
+function resetError(box, button){
+    let errorInput = box.querySelector('.error-input');
     if (errorInput) {
         errorInput.remove();
     }
@@ -35,9 +34,9 @@ function resetError(button){
 // Input 값 유효성 검사
 function validateInput(inputValue, box, errorMessage, button) {
     if (inputValue === '') {
-        displayError(box, errorMessage, button);
+            displayError(box, errorMessage, button);
     } else {
-        resetError(button);
+        resetError(box, button);
     }
 }
 
@@ -46,9 +45,9 @@ function validateInput(inputValue, box, errorMessage, button) {
 btnLogin.addEventListener('click', (event)=>{
     event.preventDefault();
 
-    const valueID = loginId.value;
-    const valuePW = password.value;
-    
+    let valueID = loginId.value;
+    let valuePW = password.value;
+
     validateInput(valueID, idBox, '아이디를 입력하세요', loginId);
     validateInput(valuePW, pwBox, '비밀번호를 입력하세요', password);
 }
