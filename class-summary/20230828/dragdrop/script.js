aebc1c99b05de9037ce0f514e6e1e0871f762e24
문정민 <130200440+jungmin801@users.noreply.github.com>
2023-08-29 22:51:01 +0900

    20230829 function update

---
+++ class-summary/20230828/dragdrop/script.js
@@ -0,0 +1,39 @@
+const dropTarget = document.querySelector('.drop-target')
+const dragItems = document.querySelectorAll('.drag-item')
+
+dragItems.forEach( item => {
+    item.addEventListener('dragstart', ()=>{
+        setTimeout(()=>{
+            item.classList.add('dragging')
+        }, 0)
+    })
+    item.addEventListener('dragend', ()=>{
+        item.classList.remove('dragging')
+    })
+})
+
+function handleDragOver(e){
+    e.preventDefault();
+
+    const draggingItem = dropTarget.querySelector('.dragging');
+    const notDraggingItems = Array.from(dropTarget.querySelectorAll('.drag-item:not(.dragging)'));
+
+    const nextItem = notDraggingItems.find((item) => {
+        // clientY : 뷰포트 내의 마우스 y좌표
+        // offsetTop : 뷰포트에서 요소까지의 거리
+        // offsetHeight : 요소의 높이
+        
+        return e.clientY <= item.offsetTop + (item.offsetHeight / 2)
+    })
+
+    console.log(nextItem)
+
+    dropTarget.insertBefore(draggingItem, nextItem)
+}
+
+
+dropTarget.addEventListener('dragover', handleDragOver)
+
+// dropTarget.addEventListener('dragenter', ()=>{
+
+// })
